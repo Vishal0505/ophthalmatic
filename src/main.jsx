afe27fd4b558d0d6e5950a450afd99095e7da801
Vishal0505 <joshivishal05adi@gmail.com>
2024-09-22 01:41:23 +0530

    feat: tailwind setup, header setup,

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@radix-ui/themes/styles.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { routeConfig } from './routefile.jsx'
+import { routeConfig } from './routefile'
 
 
 const router = createBrowserRouter(routeConfig);
