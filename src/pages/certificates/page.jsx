0270b3ffa548ae317e0aca1f0c0a24ee1fd00a7f
IammayurPatil <mayurpatil7456@gmail.com>
2024-10-06 21:27:04 +0530

    feat: added certificate page

---
+++ src/pages/certificates/page.jsx
@@ -0,0 +1,40 @@
+import { Box, Text } from "@radix-ui/themes";
+import React from "react";
+import BackgroundImage from "../../components/ui/background-image";
+import bgimg from "../../assets/bg/bgimg.jpg";
+import certi_1 from "../../assets/certificates/certi_1.png";
+import certi_2 from "../../assets/certificates/certi_2.png";
+import certi_3 from "../../assets/certificates/certi_3.png";
+import certi_4 from "../../assets/certificates/certi_4.png";
+
+export default function Certificates() {
+  return (
+    <Box className="relative bg-gradient-to-b from-blue-300 via-purple-300 to-indigo-300 inset-0 p-4">
+      <BackgroundImage imageUrl={bgimg} opacity={0.7} />
+      <div className="flex justify-center items-center relative">
+        <div className="grid grid-cols-2 grid-rows-2 items-center gap-4">
+          <img
+            src={certi_1}
+            alt="certi_1"
+            className=" w-5/6 h-full object-cover rounded-lg shadow-lg transition duration-300 gap-4"
+          />{" "}
+          <img
+            src={certi_2}
+            alt="certi_1"
+            className=" w-5/6 h-full object-cover rounded-lg shadow-lg transition duration-300 gap-4"
+          />{" "}
+          <img
+            src={certi_3}
+            alt="certi_1"
+            className=" w-5/6 h-full object-cover rounded-lg shadow-lg transition duration-300 gap-4"
+          />{" "}
+          <img
+            src={certi_4}
+            alt="certi_1"
+            className=" w-5/6 h-full object-cover rounded-lg shadow-lg transition duration-300 gap-4"
+          />
+        </div>
+      </div>
+    </Box>
+  );
+}
