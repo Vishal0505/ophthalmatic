d4e041306c2f949c57eba15ac9287936417147e1
IammayurPatil <mayurpatil7456@gmail.com>
2024-11-07 23:33:36 +0530

    fix: product page ui started

--- src/pages/products/page.jsx
+++ src/pages/products/page.jsx
@@ -2,17 +2,107 @@ import { Box, Flex, Text } from "@radix-ui/themes";
 import BackgroundImage from "../../components/ui/background-image";
 import bgimg from "../../assets/bg/bgimg.jpg";
 
+import Prod1 from "../../assets/products/prem-1.png";
+import Prod2 from "../../assets/products/prem-2.png";
+import Prod3 from "../../assets/products/prem-3.png";
+import Prod4 from "../../assets/products/prem-4.png";
+import Container from "../../components/ui/container";
+const products = [
+  {
+    id: 1,
+    name: "Ophthalmic Refraction Unit ",
+    category: "(OU-03) ",
+    imageUrl: Prod1,
+  },
+  {
+    id: 2,
+    name: " Ophthalmic Operation Table ",
+    category: "(OT - 02)",
+    imageUrl: Prod2,
+  },
+  {
+    id: 3,
+    name: "Ophthalmic Operation Table ",
+    category: "(OT - 01) ",
+    imageUrl: Prod3,
+  },
+  {
+    id: 4,
+    name: "Surgeon’s Chair ",
+    category: "(SC-01)",
+    imageUrl: Prod4,
+  },
+  {
+    id: 5,
+    name: "Ophthalmic Operation Table ",
+    category: "(OT - 01) ",
+    imageUrl: Prod1,
+  },
+  {
+    id: 6,
+    name: "Ophthalmic Refraction Unit ",
+    category: "(OU-03) ",
+    imageUrl: Prod2,
+  },
+  {
+    id: 7,
+    name: "Ophthalmic Refraction Unit ",
+    category: "(OU-03) ",
+    imageUrl: Prod2,
+  },
+  {
+    id: 8,
+    name: "Ophthalmic Refraction Unit ",
+    category: "(OU-03) ",
+    imageUrl: Prod2,
+  },
+];
 export default function ProductPage() {
   return (
-    <Box className="relative bg-gradient-to-b from-blue-200 via-purple-200 to-indigo-300 inset-0 overflow-hidden">
-      <BackgroundImage imageUrl={bgimg} opacity={0.7} />
-      <Box className="relative text-center overflow-hidden" height='100vh'>
-        <Flex direction='column' gap='4' justify='center' align='center' className=" h-full">
-          <Text size='9'>Product Page</Text>
-          <Text size='4'>Our Products Will Comming Soon.............!</Text>
+    <Box>
+      <Box className="bg-custom-gradient">
+        <Flex className="  justify-center items-center gap-2 flex-col py-10">
+          <Flex className="flex-row gap-2">
+            <Text className="text-2xl font-medium text-primary">Our</Text>
+            <Text className="text-2xl font-medium">Products</Text>
+          </Flex>
+          <Text>
+            Home / <Text className="text-primary"> Our Products </Text>
+          </Text>
         </Flex>
       </Box>
-      <Box className="absolute top-0 left-0 w-32 h-32 bg-gradient-to-l from-pink-300 to-transparent rounded-full transform -translate-x-1/2 translate-y-1/2"></Box>
-    </Box>
-  )
+    <Container>
+
+      <Box>
+        <div className="grid grid-cols-4 gap-4 py-6">
+          {products.map((item) => (
+            <div key={item.id} className="px-4 ">
+              <div className="p-4 bg-white rounded-md border border-gray-300">
+                <img
+                  src={item.imageUrl}
+                  alt={item.name}
+                  className="w-full h-56 object-cover rounded-md mb-4"
+                />
+                <Flex justify="center" direction="column">
+                  <Text
+                    align="center"
+                    className="text-base font-medium text-center "
+                  >
+                    {item.name}
+                  </Text>
+                  <Text
+                    align="center"
+                    className="text-base font-medium text-center "
+                  >
+                    {item.category}
+                  </Text>
+                </Flex>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Box>
+    </Container>
+</Box>
+  );
 }
