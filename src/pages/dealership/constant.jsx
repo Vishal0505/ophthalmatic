cd4d06052ebe1d21dde443c51a37c77d04bbf0b2
Vishal0505 <joshivishal05adi@gmail.com>
2024-12-08 22:40:45 +0530

    dealership

--- src/pages/dealership/constant.jsx
+++ src/pages/dealership/constant.jsx
@@ -1,5 +1,11 @@
 
 import grow from "../../assets/dealership/grow.png";
+import dealership_1 from "../../assets/dealership/dealership_1.png";
+import dealership_2 from "../../assets/dealership/dealership_2.png";
+import dealership_3 from "../../assets/dealership/dealership_3.png";
+import dealership_4 from "../../assets/dealership/dealership_4.png";
+import dealership_5 from "../../assets/dealership/dealership_5.png";
+import dealership_6 from "../../assets/dealership/dealership_6.png";
 import innovative from "../../assets/dealership/innovative.png";
 import support from "../../assets/dealership/support.png";
 
@@ -22,69 +28,34 @@ export const partners = [
     }
 ];
 
-
-// export const benefits = [
-//     {
-//         imgSrc: grow,
-//         title: "Exclusive Territory Rights",
-//         description: "Receive exclusive distribution rights in your designated area. This unique position ensures you are the sole authorized distributor of our products in your territory, minimizing competition and maximizing your market potential."
-//     },
-//     {
-//         imgSrc: grow,
-//         title: "Dedicated Account Management",
-//         description: "Enjoy personalized guidance from a dedicated relationship manager committed to your success. This includes regular check-ins, tailored advice, and prompt assistance to help you overcome challenges and thrive in your business."
-//     },
-//     {
-//         imgSrc: grow,
-//         title: "Profit-Driven Pricing",
-//         description: "Benefit from specially tailored pricing models designed to enhance your profit margins. Our competitive pricing structures consider market conditions and your business requirements, helping you achieve optimal profitability."
-//     },
-//     {
-//         imgSrc: grow,
-//         title: "Tailored Marketing Solutions",
-//         description: "Leverage our expertise in digital marketing with customized strategies for your market. We provide targeted online advertising, social media campaigns, and search engine optimization to help you effectively reach and engage potential customers."
-//     },
-//     {
-//         imgSrc: grow,
-//         title: "Expedited Order Fulfillment",
-//         description: "Receive priority processing and fast delivery of your orders. Our expedited fulfillment ensures that you get products quickly, allowing you to meet customer demands and maintain smooth operations."
-//     },
-//     {
-//         imgSrc: grow,
-//         title: "Custom Branding Support",
-//         description: "Boost your dealership’s visibility with personalized branding support. This includes custom stationery, promotional items, and gifts, all designed to reinforce your brand identity and create a lasting impression with your customers."
-//     }
-// ];
-
-
 export const benefits = [
     {
-        imgSrc: grow,
+        imgSrc: dealership_1,
         title: "Exclusive Territory Rights",
         description: "Gain exclusive distribution rights in designated area. This ensures  the sole authorized distributor of our products, reducing competition and maximizing your market potential for growth."
     },
     {
-        imgSrc: grow,
+        imgSrc: dealership_2,
         title: "Dedicated Account Management",
         description: "Work with a dedicated account manager who provides personalized support, regular check-ins, and tailored advice to help you overcome challenges and thrive in your business."
     },
     {
-        imgSrc: grow,
+        imgSrc: dealership_3,
         title: "Profit-Driven Pricing",
         description: "Benefit from special pricing models designed to maximize your profit margins. We provide competitive pricing tailored to market conditions and your unique business needs for optimal profitability."
     },
     {
-        imgSrc: grow,
+        imgSrc: dealership_4,
         title: "Tailored Marketing Solutions",
         description: "Leverage our expertise in digital marketing with custom strategies, including targeted ads, social media campaigns, and SEO to effectively engage your audience and drive growth."
     },
     {
-        imgSrc: grow,
+        imgSrc: dealership_5,
         title: "Expedited Order Fulfillment",
         description: "Receive priority processing and fast delivery for your orders. Our expedited fulfillment ensures timely product delivery, helping you meet customer demands and maintain efficient operations."
     },
     {
-        imgSrc: grow,
+        imgSrc: dealership_6,
         title: "Custom Branding Support",
         description: "Boost your brand’s visibility with personalized branding support. This includes custom stationery, promotional items, and gifts, all designed to reinforce your brand identity."
     }
